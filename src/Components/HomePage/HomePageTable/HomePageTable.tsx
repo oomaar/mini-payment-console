e510b4ee7fff80613d83458816056171dc4a2dcf
oomaar <omaar_5@hotmail.com>
2025-09-16 14:10:04 +0300

    add HomePageTable component with styled table layout

---
+++ src/Components/HomePage/HomePageTable/HomePageTable.tsx
@@ -0,0 +1,39 @@
+import { Payment } from "@/lib/db";
+import "./HomePageTable.css";
+
+type HomePageTableProps = {
+  filtered: Payment[];
+};
+
+export function HomePageTable(props: HomePageTableProps) {
+  const { filtered } = props;
+
+  return (
+    <div className="table-container">
+      <table className="table-table">
+        <thead>
+          <tr>
+            <th>Merchant Order ID</th>
+            <th>Amount</th>
+            <th>Status</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filtered.map((p) => (
+            <tr key={p.id}>
+              <td>
+                <a href={`/payments/${p.id}`}>{p.merchantOrderId}</a>
+              </td>
+              <td>
+                {p.amount / 100} {p.currency}
+              </td>
+              <td>{p.status}</td>
+              <td>{new Date(p.createdAt).toLocaleString("en-UK")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
