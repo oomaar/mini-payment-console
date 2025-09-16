eeaedbef8a86554627331934482c72204f94e15e
oomaar <omaar_5@hotmail.com>
2025-09-16 23:35:40 +0300

    Add PayPage with payment details and ability to mark as paid or canceled

---
+++ src/app/pay/[publicId]/page.tsx
@@ -0,0 +1,72 @@
+import { getPaymentByPublicId } from "@/lib/db";
+import { markPaid, markCanceled } from "@/actions/payments";
+import "./PayPublicId.css";
+
+type PayPageProps = {
+  params: { publicId: string };
+};
+
+export default async function PayPage(props: PayPageProps) {
+  const { params } = props;
+
+  const payment = await getPaymentByPublicId(params.publicId);
+
+  if (!payment)
+    return (
+      <div className="pay-public-id-page-empty-container">
+        Payment not found.
+      </div>
+    );
+
+  return (
+    <div className="pay-public-id-page-container">
+      <div className="pay-public-id-page-sub-container">
+        <h1 className="pay-public-id-page-title">
+          Pay for Order {payment.merchantOrderId}
+        </h1>
+
+        <div className="pay-public-id-page-form">
+          <p className="pay-public-id-page-detail-text">
+            Amount:{" "}
+            <span>
+              {payment.amount / 100} {payment.currency}
+            </span>
+          </p>
+          <p className="pay-public-id-page-detail-text">
+            Status: <span>{payment.status}</span>
+          </p>
+
+          {payment.status === "pending" ? (
+            <div className="pay-public-id-page-footer">
+              <form
+                action={async () => {
+                  "use server";
+                  await markPaid(params.publicId);
+                }}
+              >
+                <button type="submit">Mark Paid</button>
+              </form>
+              <form
+                action={async () => {
+                  "use server";
+                  await markCanceled(params.publicId);
+                }}
+              >
+                <button
+                  type="submit"
+                  className="pay-public-id-page-footer-cancel-button"
+                >
+                  Cancel
+                </button>
+              </form>
+            </div>
+          ) : (
+            <p>
+              This payment is already <b>{payment.status}</b>.
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
